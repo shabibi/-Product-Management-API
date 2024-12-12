c75627cdb7421aa1459ac016a538fc9e46a38f83
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-12 12:38:47 +0400

    product database setup

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementAPI.Models;
+
+namespace ProductManagementAPI
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagementAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int PID { get; set; }
+
+        public string PName {  get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
