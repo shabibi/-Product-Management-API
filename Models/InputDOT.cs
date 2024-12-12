db345c66f72b1a28c27391fc1b5c156f31d95167
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-12 12:39:23 +0400

    DOT Models

---
+++ Models/InputDOT.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagementAPI.Models
+{
+    public class InputDOT
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range (1, int.MaxValue)]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = "general";
+    }
+}
---
+++ Models/OutputDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductManagementAPI.Models
+{
+    public class OutputDTO
+    {
+        public string PName { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
