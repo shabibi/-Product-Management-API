c096514456b4229a42e822b9e50677bf0d3dd285
Afra Alshabibi <a.shabibi20@gmail.com>
2024-12-12 13:22:12 +0400

    Product Service

---
+++ Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ProductManagementAPI.Models;
+
+namespace ProductManagementAPI.Services
+{
+    public interface IProductService
+    {
+        void AddNewProduct(Product product);
+        void DeleteProduct(int id);
+        IEnumerable<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void UpdateProduct(Product product);
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using ProductManagementAPI.Models;
+using ProductManagementAPI.Repositories;
+
+namespace ProductManagementAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepo _productRepo;
+
+        public ProductService(IProductRepo productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        public void AddNewProduct(Product product)
+        {
+            _productRepo.AddNewProduct(product);
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _productRepo.GetAllProducts();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _productRepo.GetProductById(id);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _productRepo.UpdateProduct(product);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            _productRepo.DeleteProduct(id);
+        }
+    }
+}
